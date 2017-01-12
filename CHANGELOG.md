f0b2f68ea91cd5521f1658e8c95c503b24717f5b
Karl Hughes <khughes.me@gmail.com>
2017-01-12 14:32:25 -0600

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+All Notable changes to `jobs-craigslist` will be documented in this file
+
+## 0.1.0 - 2017-01-12
+
+### Added
+- Initial Release
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
---
+++ src/Queries/MonsterQuery.php
@@ -0,0 +1,63 @@
+<?php namespace JobApis\Jobs\Client\Queries;
+
+class MonsterQuery extends AbstractQuery
+{
+    /**
+     * q
+     *
+     * The search query.
+     *
+     * @var string
+     */
+    protected $q;
+
+    /**
+     * where
+     *
+     * The search location.
+     *
+     * @var string
+     */
+    protected $where;
+
+    /**
+     * page
+     *
+     * Page
+     *
+     * @var integer
+     */
+    protected $page;
+
+    /**
+     * Get baseUrl
+     *
+     * @return  string Value of the base url to this api
+     */
+    public function getBaseUrl()
+    {
+        return 'http://rss.jobsearch.monster.com/rssquery.ashx';
+    }
+
+    /**
+     * Get keyword
+     *
+     * @return  string Attribute being used as the search keyword
+     */
+    public function getKeyword()
+    {
+        return $this->q;
+    }
+
+    /**
+     * Required parameters
+     *
+     * @return array
+     */
+    protected function requiredAttributes()
+    {
+        return [
+            'q',
+        ];
+    }
+}
